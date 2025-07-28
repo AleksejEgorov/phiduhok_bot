5abe83a8e740e8160f30f775c3afc1db90a74125
Aleksej Egorov <Egorov.Aleksej.V@outlook.com>
2025-07-28 12:57:46 +0300

    Init.

---
+++ main.py
@@ -0,0 +1,188 @@
+
+import os
+import logging
+import asyncio
+import sqlite3
+import random
+import re
+import yaml
+from telebot.async_telebot import AsyncTeleBot
+
+logger = logging.getLogger(__name__)
+BOT_NAME = 'phiduhok_bot'
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+# Import conf
+with open('config.yaml', encoding='utf-8') as conf_file:
+    conf: dict = yaml.load(conf_file, Loader=yaml.FullLoader)
+
+bot = AsyncTeleBot(conf['telegram_token'])
+content_dir = conf['content_dir']
+db_path = conf['db_path']
+connection = sqlite3.connect(db_path)
+cursor = connection.cursor()
+
+
+
+
+
+async def get_meme_file(meme_caption=''):
+    """
+    Get a random meme file from the content directory.
+    """
+    memes_count = cursor.execute('SELECT COUNT(*) FROM memes').fetchone()[0]
+
+    if memes_count == 0:
+        logger.warning('No memes found in %s', content_dir)
+        return None
+    if meme_caption:
+        memes = cursor.execute(
+            '''
+            SELECT file_path FROM memes
+            WHERE caption LIKE ?
+            ORDER BY RANDOM()
+            LIMIT 1
+            ''', ('%' + meme_caption + '%',)
+        ).fetchall()
+    else:
+        memes = cursor.execute('''
+            SELECT file_path FROM memes
+            ORDER BY RANDOM()
+            LIMIT 1
+        ''').fetchone()
+
+    logger.info('%s memes found', len(memes))
+    if len(memes) > 1:
+        meme = random.choice(memes)[0]
+    elif len(memes) == 1:
+        meme = memes[0][0]
+    else:
+        meme = None
+
+    if meme:
+        meme_file = os.path.join(conf['content_dir'],meme)
+        logger.info('Selected meme file: %s', meme_file)
+        return meme_file
+    return None
+
+
+@bot.message_handler(commands=['start'])
+async def start_message(message):
+    '''
+    Process /start command
+    '''
+    await bot.send_message(
+        message.chat.id,
+        'Ну?'
+    )
+
+@bot.message_handler(regexp="(дай|хочу|покажи|пришли) мем")
+async def handle_message(message):
+    """
+    Handle memes request.
+    """
+    if message.chat.type == "private" or message.text.startswith('@' + BOT_NAME):
+        logger.info('Received message: %s', message.text)
+        meme_caption_request = re.sub(
+            r'^.+ мем',
+            '',
+            message.text,
+            flags=re.IGNORECASE
+        ).strip().lower()
+
+        meme_file = await get_meme_file(meme_caption_request)
+        print(meme_file)
+        if meme_file:
+            with open(meme_file, 'rb') as file:
+                await bot.send_photo(
+                    message.chat.id,
+                    file
+                )
+        else:
+            await bot.send_message(
+                message.chat.id,
+                'Mемов не завезли!'
+            )
+
+@bot.message_handler(content_types=['photo'])
+async def handle_photo(message):
+    """
+    Handle incoming photos.
+    """
+    if message.chat.type == "private" or message.caption.startswith('@' + BOT_NAME):
+        if message.from_user.id not in conf['allowed_ids']:
+            await bot.send_message(
+                message.chat.id,
+                f"`{message.from_user.id}` и ты знаешь, куда с этим идти!"
+            )
+            return
+        logger.info('Received photo from %s', message.from_user.username)
+        file_info = await bot.get_file(message.photo[-1].file_id)
+        file_path = file_info.file_path
+        downloaded_file = await bot.download_file(file_path)
+
+        meme_file_name = f'{message.photo[-1].file_id}.jpg'
+        meme_file = os.path.join(content_dir, meme_file_name)
+        with open(meme_file, 'wb') as new_file:
+            new_file.write(downloaded_file)
+
+        cursor.execute(
+            '''
+            INSERT INTO memes (file_path, caption)
+            VALUES (?, ?)
+            ''', (
+                meme_file_name,
+                message.caption.replace('@' + BOT_NAME, '').strip().lower()
+            )
+        )
+        connection.commit()
+
+        logger.info('Photo saved to %s', meme_file)
+        await bot.send_message(
+            message.chat.id,
+            'Ну!'
+        )
+
+@bot.message_handler(
+    func=lambda message: True,
+    content_types=[
+        'audio',
+        'voice',
+        'video',
+        'document',
+        'location',
+        'contact',
+        'sticker'
+    ]
+)
+async def handle_other(message):
+    """
+    Handle other mediatypes.
+    """
+    if message.text.startswith('@' + BOT_NAME) or message.chat.type == "private":
+        logger.warning('No photo found in message: %s', message.text)
+        await bot.send_message(
+            message.chat.id,
+            'Только картинки принимаем!'
+        )
+
+
+if __name__ == '__main__':
+    # create logger
+    logging.basicConfig(
+        level=str(conf['log_level']).upper(),
+        format=conf['log_format']
+    )
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS memes (
+    id INTEGER PRIMARY KEY,
+    file_path TEXT NOT NULL,
+    caption TEXT
+    )
+    ''')
+    cursor.execute('CREATE INDEX IF NOT EXISTS idx_caption ON memes (caption)')
+    connection.commit()
+
+    logger.info('Bot online')
+    asyncio.run(bot.infinity_polling())
+    connection.close()
