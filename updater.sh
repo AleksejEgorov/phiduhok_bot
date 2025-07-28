859c30b1ead29d678ce45d2cbce3c927321ff709
Aleksej Egorov <Egorov.Aleksej.V@outlook.com>
2025-07-28 13:59:17 +0300

    recreate venv

--- updater.sh
+++ updater.sh
@@ -2,6 +2,8 @@
 cd /opt/phiduhok_bot
 systemctl stop phiduhok-bot
 sudo -u botrunner git pull --rebase
+#rm -rf /opt/phiduhok_bot/.venv
+#sudo -u botrunner python3 -m venv .venv
 sudo -u botrunner /opt/phiduhok_bot/.venv/bin/pip install -r requirements.txt --no-cache-dir
 chown botrunner:botrunner /opt/phiduhok_bot -R
 systemctl start phiduhok-bot
